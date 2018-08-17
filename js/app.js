b24acab037d3d3eefaec4e45a6820a06c77e6ae6
Erik Wilder <erikkurtu@gmail.com>
2018-08-17 12:55:04 -0400

    App 1b updates

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("hello beast!");
+$(document).ready(function() {
+  console.log("hello beast!");
+  $("#addItemButton").click(function() {
+    var itemInputValue = $("#itemName").val();
+    $("#itemList").append("<li class='list-group-item listItem'>"+itemInputValue+" <button type='button' class='btn btn-danger deleteButton'>X</button></li><br>");
+    $("#itemName").val("");
+  });
+  $('body').on('click', '.deleteButton', function() {
+    $(this).parent().remove();
+  });
+  $('body').on('click', '.listItem', function() {
+    $(this).css("background-color", "#666666");
+    $(this).insertAfter($('#itemList').children().last());
+  });
+});
