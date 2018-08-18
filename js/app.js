bc7c80452422c66ebe91511216e18eea544b665f
Ben Bringardner <ben.bringardner@gmail.com>
2018-08-18 00:11:52 -0500

    Double Click option working
    Double click option works now too

--- js/app.js
+++ js/app.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   console.log("hello beast!");
   $("#addItemButton").click(function() {
     var itemInputValue = $("#itemName").val();
-    $("#itemList").append("<li class='list-group-item listItem'>"+itemInputValue+" <button type='button' class='btn btn-danger deleteButton'>X</button>"+" <button type='button' class='btn btn-secondary doneButton'>done</button></li><br>");
+    $("#itemList").append("<li class='list-group-item listItem'>"+itemInputValue+" <button type='button' class='btn btn-danger deleteButton'>X</button></li><br>");
     $("#itemName").val("");
   });
-  $('body').on('click', '.doneButton', function() {
-    $(this).parent().css("background-color", "#666666");
-    $(this).parent().insertAfter($('#itemList').children().last());
+  $('body').on('dblclick', '.listItem', function() {
+    $(this).css("background-color", "#666666");
+    $(this).insertAfter($('#itemList').children().last());
   });
   $('body').on('click', '.deleteButton', function() {
     $(this).parent().remove();
