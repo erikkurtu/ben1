751be29073b86c6b2848026ed2c883e4c1ac9cab
Erik WIlder <erikkurtu@gmail.com>
2018-08-22 17:46:52 -0400

    basic localforage functions

--- js/app.js
+++ js/app.js
@@ -1,8 +1,29 @@
 $(document).ready(function() {
   console.log("hello beast!");
+  var itemList = [];
+  function loadSavedItems () {
+    localforage.getItem('benapp').then(function(benappValue) {
+      if (typeof benappValue !== 'undefined' && benappValue !== null) {
+        itemList = benappValue;
+        _.each(benappValue, function (bav) {
+          $("#itemList").append("<li class='list-group-item listItem'><span class='listText'>"+bav+"</span> <button type='button' class='btn btn-danger deleteButton'>X</button></li><br>");
+        });
+      }
+    }).catch(function(err) {
+        console.log(err);
+    });
+  }
+  loadSavedItems();
+
   $("#addItemButton").click(function() {
     var itemInputValue = $("#itemName").val();
-    $("#itemList").append("<li class='list-group-item listItem'>"+itemInputValue+" <button type='button' class='btn btn-danger deleteButton'>X</button></li><br>");
+    $("#itemList").append("<li class='list-group-item listItem'><span class='listText'>"+itemInputValue+"</span> <button type='button' class='btn btn-danger deleteButton'>X</button></li><br>");
+    itemList.push(itemInputValue);
+    localforage.setItem('benapp', itemList).then(function (value) {
+
+    }).catch(function(err) {
+        console.log(err);
+    });
     $("#itemName").val("");
   });
   $('body').on('click', '.listItem', function() {
@@ -11,6 +32,15 @@ $(document).ready(function() {
   });
   $('body').on('click', '.deleteButton', function() {
     $(this).parent().remove();
+    var listTextValue = $(this).parent().children('.listText').text();
+    var listItemIndex = itemList.indexOf(listTextValue);
+    console.log(listItemIndex);
+    itemList.splice(listItemIndex, 1);
+    localforage.setItem('benapp', itemList).then(function (value) {
+
+    }).catch(function(err) {
+        console.log(err);
+    });
     e.stopPropagation();
   });
 });
